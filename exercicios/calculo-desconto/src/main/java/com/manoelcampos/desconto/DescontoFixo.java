20495601a2e60a359ba97689327b34fe007c97ee
LuanaLB <lunasbatista0@gmail.com>
2024-07-01 09:33:00 -0300

    readme

--- exercicios/calculo-desconto/src/main/java/com/manoelcampos/desconto/DescontoFixo.java
+++ exercicios/calculo-desconto/src/main/java/com/manoelcampos/desconto/DescontoFixo.java
@@ -3,14 +3,10 @@ package com.manoelcampos.desconto;
 public class DescontoFixo implements DescontoStrategy{
     private double percentual;
 
-    public  DescontoFixo(double percentual) {
-
-        this.percentual = percentual;
-    }
+    public  DescontoFixo(double percentual) {this.percentual = percentual;}
 
     @Override
     public double calcularDesconto(double valorTotal, Cliente cliente) {
-
         return valorTotal * (percentual / 100);
     }
 }
